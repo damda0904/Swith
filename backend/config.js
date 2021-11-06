305876b65267e9585f82b51c1f4940370f6df5bc
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-06 19:18:47 +0900

    폴더 오류 고치는 중

---
+++ backend/config.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+//환경변수 유무를 체크하는 함수
+function required(key, defaultValue = undefined) {
+    const value = process.env[key] || defaultValue;
+    if (value == null) {
+        //throw new Error(`Key ${key} is undefined`)
+    }
+    return value;
+}
+
+export const config = {
+    host: {
+        port: parseInt(required('HOST_PORT', 8080))
+    },
+    gmail: {
+        address: required('GMAIL_ADDRESS'),
+        passwd: required('GMAIL_PASSWORD'),
+    }
+}
