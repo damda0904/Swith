d52ebef848fc0ea98119d10c3eb9b6bd21f58b00
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-11-07 02:30:33 +0900

    feat:sign up layout

--- frontend/App.js
+++ frontend/App.js
@@ -16,8 +16,8 @@ const Stack = createStackNavigator();
 const Auth = () => {
   return(
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}/>
+      <Stack.Screen name="Signup" component={SignupScreen} options={{headerShown: false}}/>
     </Stack.Navigator>
   )
 }
--- frontend/src/components/screens/SignupScreen.js
+++ frontend/src/components/screens/SignupScreen.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{useState} from 'react'
+import { StyleSheet, View, Text, TextInput } from 'react-native'
+import InputEmail from '../screenComponents/Auth/InputEmail';
+import ConfirmEmail from '../screenComponents/Auth/ConfirmEmail';
+import SignupInfo from '../screenComponents/Auth/SignupInfo';
+import { createStackNavigator } from "@react-navigation/stack";
 
-const SignUp = () => {
+
+const SignUpScreen = () => {
+    const Stack = createStackNavigator();
+
+    const [email,setEmail] = useState('');
+    
     return (
-        <View>
-            <Text>sign up</Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name="InputEmail" component={InputEmail} options={{headerShown: false}}/>
+            <Stack.Screen name="ConfirmEmail" component={ConfirmEmail} options={{headerShown: false}}/>
+            <Stack.Screen name="SignupInfo" component={SignupInfo} options={{headerShown: false}}/>
+        </Stack.Navigator>
     )
 }
 
-export default SignUp
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color:'black',
+        paddingHorizontal:20
+    },
+});
+
+export default SignUpScreen
